30d4df178eb728aa3bb9ce1bf45dc82f2649a17d
dnhuan <huandngoc@gmail.com>
2022-12-19 00:49:02 -0700

    Finished event listener

--- src/content/content.js
+++ src/content/content.js
@@ -1,139 +1,183 @@
-window.addEventListener("load", waitForTableFetch, false);
+window.addEventListener("load", waitForFirstLoad, false);
 
-function waitForTableFetch() {
-  let tableTimer = setInterval(checkForTableInDOM, 50);
+function waitForFirstLoad() {
+	let tableTimer = setInterval(checkForTableInDOM, 50);
 
-  function checkForTableInDOM() {
-    if (document.querySelector("#CatalogList")) {
-      clearInterval(tableTimer);
-      main();
-    }
-  }
+	function checkForTableInDOM() {
+		if (document.getElementsByClassName("class-results").length > 0) {
+			clearInterval(tableTimer);
+			startObserver();
+		}
+	}
+}
+
+function log(message) {
+	console.log(`%cRMAP`, "color: #26bfa5;", message);
+}
+
+function startObserver() {
+	log("Page fully loaded");
+	// trigger first time
+	main();
+
+	// Set mutation observer on loading spinner to wait for table reload
+	const observer = new MutationObserver((mutations) => {
+		mutations.forEach((mutation) => {
+			// check if a node is removed in this mutation
+			log(mutation);
+			if (mutation.removedNodes.length > 0) {
+				// check if the removed node is the loading spinner
+				if (
+					mutation.removedNodes[0].className?.includes(
+						"spinner-button"
+					)
+				) {
+					log("Loading spinner removed");
+					// wait for table to load
+					setTimeout(() => {
+						log("Table loaded");
+						main();
+					}, 1000);
+				}
+			}
+		});
+	});
+
+	const reactRootDOM = document.getElementById("root");
+
+	observer.observe(reactRootDOM, { subtree: true, childList: true });
 }
 
 function main() {
-  console.log("Page fully loaded");
+	// check if there is h1 tag with content "No classes found"
+	if ($('h2:contains("No classes found")').length > 0) {
+		log("NONE FOUND");
+		return;
+	}
+	log("FOUND");
+}
 
-  let domList = [];
-  let profDataSet = [];
+function getDataFromDOM() {
+	let domList = [];
+	let profDataSet = [];
 
-  // Get all rows from table
-  $("#CatalogList > tbody > tr").each((idx, elm) => {
-    domList.push(elm);
-  });
+	// Get all rows from table
+	$("#CatalogList > tbody > tr").each((idx, elm) => {
+		domList.push(elm);
+	});
 
-  // Parse professors data - get name
-  let profNameList = domList.map((elm) => {
-    let profName = parseProfName(elm);
-    if (profDataSet.indexOf(profName) == -1) {
-      profDataSet.push(profName);
-    }
-    return {
-      domElem: elm,
-      name: parseProfName(elm),
-    };
-  });
+	// Parse professors data - get name
+	let profNameList = domList.map((elm) => {
+		let profName = parseProfName(elm);
+		if (profDataSet.indexOf(profName) == -1) {
+			profDataSet.push(profName);
+		}
+		return {
+			domElem: elm,
+			name: parseProfName(elm),
+		};
+	});
 
-  // Process data set
-  profDataSet.map((prof) => fetchAndAppendProfData(prof, profNameList));
+	// Process data set
+	profDataSet.map((prof) => fetchAndAppendProfData(prof, profNameList));
 }
 
 function parseProfName(elm) {
-  let raw = elm.className;
-  let rawData = raw.split("-");
-  return `${rawData[4]} ${rawData[5]}`;
+	let raw = elm.className;
+	let rawData = raw.split("-");
+	return `${rawData[4]} ${rawData[5]}`;
 }
 
 function compareProfName(profName, queryName) {
-  let simlilarity = stringSimilarity.compareTwoStrings(
-    profName,
-    `${queryName.firstName} ${queryName.lastName}`
-  );
-  if (simlilarity >= 0.8) {
-    return true;
-  }
-  return false;
+	let simlilarity = stringSimilarity.compareTwoStrings(
+		profName,
+		`${queryName.firstName} ${queryName.lastName}`
+	);
+	if (simlilarity >= 0.8) {
+		return true;
+	}
+	return false;
 }
 
 async function fetchAndAppendProfData(name, profNameList) {
-  let profID = await fetchProfID(name);
-  let profData = await fetchProfDataFromID(profID);
-  if (profData === null) {
-    console.log("Professor data not found: ", name);
-    return;
-  }
+	let profID = await fetchProfID(name);
+	let profData = await fetchProfDataFromID(profID);
+	if (profData === null) {
+		console.log("Professor data not found: ", name);
+		return;
+	}
 
-  // display data on frontend
-  profNameList.forEach((prof) => {
-    if (compareProfName(prof.name, profData)) {
-      appendProfDataToDOM(prof.domElem, profData);
-    }
-  });
+	// display data on frontend
+	profNameList.forEach((prof) => {
+		if (compareProfName(prof.name, profData)) {
+			appendProfDataToDOM(prof.domElem, profData);
+		}
+	});
 }
 
 function appendProfDataToDOM(domElem, profData) {
-  if (profData.numRatings == 0) {
-    return;
-  }
-  let colorFont = "#0F0F0F";
-  let colorCode = "";
-  if (profData.avgRating < 2.5) {
-    colorCode = "#FF9C9C";
-  } else if (profData.avgRating < 3.5) {
-    colorCode = "#FFFF68";
-  } else {
-    colorCode = "#68FFBE";
-  }
-  const divFormat = `<div style="background-color:${colorCode}">
+	if (profData.numRatings == 0) {
+		return;
+	}
+	let colorFont = "#0F0F0F";
+	let colorCode = "";
+	if (profData.avgRating < 2.5) {
+		colorCode = "#FF9C9C";
+	} else if (profData.avgRating < 3.5) {
+		colorCode = "#FFFF68";
+	} else {
+		colorCode = "#68FFBE";
+	}
+	const divFormat = `<div style="background-color:${colorCode}">
   <a style="color:${colorFont}" target="_blank" href="https://www.ratemyprofessors.com/ShowRatings.jsp?tid=${
-    profData.legacyId
-  }">
+		profData.legacyId
+	}">
     <div><span style="font-size:2em;font-weight: bold;">${
-      profData.avgRating
-    }</span>/5</div>
+		profData.avgRating
+	}</span>/5</div>
     <div>Average difficulty: ${profData.avgDifficulty}</div>
     <div>${profData.wouldTakeAgainPercent.toFixed(0)}% would take again</div>
     <div>${profData.numRatings} rating(s)</div>
   </a>
   </div>`;
 
-  $(domElem).find(".instructorListColumnValue").append(divFormat);
+	$(domElem).find(".instructorListColumnValue").append(divFormat);
 }
 
 async function fetchProfID(name) {
-  try {
-    let response = await sendMessage({
-      contentScriptQuery: "queryProfID",
-      profName: name,
-    });
-    let profID = response.data.newSearch.teachers.edges[0].node.id;
-    return profID;
-  } catch (error) {
-    return null;
-  }
+	try {
+		let response = await sendMessage({
+			contentScriptQuery: "queryProfID",
+			profName: name,
+		});
+		let profID = response.data.newSearch.teachers.edges[0].node.id;
+		return profID;
+	} catch (error) {
+		return null;
+	}
 }
 
 async function fetchProfDataFromID(ID) {
-  if (ID === null) {
-    return null;
-  }
-  try {
-    let response = await sendMessage({
-      contentScriptQuery: "queryProfData",
-      profID: ID,
-    });
-    let profData = response.data.node;
-    return profData;
-  } catch (error) {
-    return null;
-  }
+	if (ID === null) {
+		return null;
+	}
+	try {
+		let response = await sendMessage({
+			contentScriptQuery: "queryProfData",
+			profID: ID,
+		});
+		let profData = response.data.node;
+		return profData;
+	} catch (error) {
+		return null;
+	}
 }
 
 function sendMessage(message) {
-  return new Promise((resolve, _) => {
-    chrome.runtime.sendMessage(message, (res) => {
-      console.log(JSON.stringify(res));
-      resolve(res);
-    });
-  });
+	return new Promise((resolve, _) => {
+		chrome.runtime.sendMessage(message, (res) => {
+			console.log(JSON.stringify(res));
+			resolve(res);
+		});
+	});
 }
