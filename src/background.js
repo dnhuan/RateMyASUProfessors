ea4c711b0694c419a155ad1086d36831f9f4a078
dnhuan <huandngoc@gmail.com>
2022-12-20 10:04:35 -0700

    Added fallback queries for multiple campuses

--- src/background.js
+++ src/background.js
@@ -1,5 +1,11 @@
 const AUTHORIZATION_TOKEN = "Basic dGVzdDp0ZXN0";
-const SCHOOL_ID = "U2Nob29sLTQ1";
+const SCHOOL_IDS = [
+	"U2Nob29sLTQ1",
+	"U2Nob29sLTEzMjcx",
+	"U2Nob29sLTEzNDgz",
+	"U2Nob29sLTEzNjQ3",
+	"U2Nob29sLTE3MTA4",
+];
 const PROFESSOR_ID = `
 query ($query: TeacherSearchQuery!) {
     newSearch {
@@ -32,44 +38,67 @@ query ($id: ID!) {
 }
 `;
 
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  switch (request.contentScriptQuery) {
-    case "queryProfID":
-      fetch("https://www.ratemyprofessors.com/graphql", {
-        method: "POST",
-        headers: {
-          Authorization: AUTHORIZATION_TOKEN,
-        },
-        body: JSON.stringify({
-          query: PROFESSOR_ID,
-          variables: {
-            query: { text: request.profName, schoolID: SCHOOL_ID },
-          },
-        }),
-      })
-        .then((res) => res.json())
-        .then((res) => sendResponse(res))
-        .catch((err) => sendResponse(new Error(err)));
-      return true;
+const queryProfID = async function queryProfIDAsync(profName, sendResponse) {
+	try {
+		let response = null;
 
-    case "queryProfData":
-      fetch("https://www.ratemyprofessors.com/graphql", {
-        method: "POST",
-        headers: {
-          Authorization: AUTHORIZATION_TOKEN,
-        },
-        body: JSON.stringify({
-          query: PROFESSOR_DATA,
-          variables: {
-            id: request.profID,
-          },
-        }),
-      })
-        .then((res) => res.json())
-        .then((res) => sendResponse(res))
-        .catch((err) => sendResponse(new Error(err)));
-      return true;
-    default:
-      return true;
-  }
+		for (let i = 0; i < SCHOOL_IDS.length; i++) {
+			const SCHOOL_ID = SCHOOL_IDS[i];
+			const raw_response = await fetch(
+				"https://www.ratemyprofessors.com/graphql",
+				{
+					cache: "force-cache",
+					method: "POST",
+					headers: {
+						Authorization: AUTHORIZATION_TOKEN,
+					},
+					body: JSON.stringify({
+						query: PROFESSOR_ID,
+						variables: {
+							query: { text: profName, schoolID: SCHOOL_ID },
+						},
+					}),
+				}
+			);
+
+			response = await raw_response.json();
+			if (response.data.newSearch.teachers.edges.length !== 0) {
+				break;
+			}
+		}
+
+		sendResponse(response);
+	} catch (error) {
+		sendResponse(new Error(error));
+	}
+};
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+	switch (request.contentScriptQuery) {
+		case "queryProfID":
+			queryProfID(request.profName, sendResponse);
+			return true;
+
+		case "queryProfData":
+			fetch("https://www.ratemyprofessors.com/graphql", {
+				cache: "force-cache",
+				method: "POST",
+				headers: {
+					Authorization: AUTHORIZATION_TOKEN,
+				},
+				body: JSON.stringify({
+					query: PROFESSOR_DATA,
+					variables: {
+						id: request.profID,
+					},
+				}),
+			})
+				.then((res) => res.json())
+				.then((res) => sendResponse(res))
+				.catch((err) => sendResponse(new Error(err)));
+			return true;
+
+		default:
+			return true;
+	}
 });
